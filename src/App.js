ad304f3e75343c290c7a2ed54805a3bc84481d01
Marcin Kacperski <kacperski81@gmail.com>
2023-02-15 22:45:12 +0000

    deleted unused state variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { useState, useEffect } from 'react';
 import Modal from './Components/Modal';
 import './App.css';
-import { has } from 'lodash';
 
 function App() {
   const [isModalOpen, setModalOpen] = useState(false);
   const toggleModal = () => setModalOpen(!isModalOpen);
   const [starMap,setStarMap] = useState('');
-  const [isLoading,setIsLoading] = useState(true);
-  const [swData,setSwData] = useState([]);
   const applicationId = "eca02537-5f0d-4488-b966-92971fb9735b";
   const applicationSecret = "88982040aa5a1815af1359421d028688df5b2728985734da3953e5dcc014f16f907e3f75ed723dc2feec4768eafec0f1b0f4a39c3f119dea6a74b8291adb0e6a745d26f241331bfc36fd9a5e6ec5d55cbe85fe2eec79423f537f7aa6f95a9ce926e14b5be8fb72ea56ea2ad4710c6f6e"
   const hash = btoa(`${applicationId}:${applicationSecret}`);
