8ec24821a8f574aa1df71462c83966624d5dcc71
vrabcho7 <kamen.ivanov@gmail.com>
2023-02-16 20:34:25 +0000

    fix nasa image for netlify

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
   const [isModalOpen, setModalOpen] = useState(false);
   const toggleModal = () => setModalOpen(!isModalOpen);
 
-  useEffect(() => setNasaImage,[])
+  useEffect(() => {
+    setNasaImage();
+  },[]);
     
   return (
     <div className='App'>
