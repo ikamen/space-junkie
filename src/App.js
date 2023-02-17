68d56e858d9cad014add9b059aecf0ec50015d39
Pavel Naumovich <pavelnaumovic@gmail.com>
2023-02-17 18:31:10 +0000

    rebasing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Modal from "./Components/Modal";
 import Map from "./Components/Map";
 import moment from "moment";
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,31 @@
-import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from "../images/logoHQ.svg";
+
+const styles = {
+  container: { paddingTop: ".5em", border: "1px solid red" },
+  nav: { marginLeft: "auto" },
+  img: { height: "clamp(1.7rem, 3.5vw, 4rem)" },
+  navLink: { fontSize: "clamp(1rem, 1.5vw, 4rem)", alignItems: "center" },
+};
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br />
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid style={styles.container}>
+          <Navbar.Brand href="#home">
+            <img style={styles.img} src={logo} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav style={{ ...styles.nav, ...styles.navLink }}>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#home">Celestial Body Info</Nav.Link>
+              <Nav.Link href="#home">Future Space Missions</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
